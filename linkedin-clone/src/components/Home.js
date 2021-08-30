0841848429262640b1fd9d398bd748f6db59af8e
ManpreetSandhu <Manpreetsan12345@gmail.com>
2021-08-29 23:30:18 -0400

    Added more 3 grid sections/cards on homepage (profile card-leftside, post card-main, hastags and  recommendation - rightside

--- linkedin-clone/src/components/Home.js
+++ linkedin-clone/src/components/Home.js
@@ -2,57 +2,82 @@
 author: Manpreet Sandhu
 File: Home.js
 */
-import styled from 'styled-components';
+import styled from "styled-components";
+import LeftSide from "./LeftSide";
+import Main from "./Main";
+import RightSide from "./RightSide";
 
 const Home = (props) => {
-     return (
-          <Container>
-               <Section>
-                    <p>Urgent Hiring! Innovative, tech company seeks talented Developers.&nbsp;</p>
-                    <h5><a>Apply today?</a></h5>
-               </Section>
-          </Container>
-     );
+  return (
+    <Container>
+      <Section>
+        <p>
+          Urgent Hiring! Innovative, tech company seeks talented
+          Developers.&nbsp;
+        </p>
+        <h5>
+          <a>Apply today?</a>
+        </h5>
+      </Section>
+      <Layout>
+        <LeftSide />
+        <Main />
+        <RightSide />
+      </Layout>
+    </Container>
+  );
 };
 
 const Container = styled.div`
- padding-top: 52px;
- max-width: 100%;
+  padding-top: 52px;
+  max-width: 100%;
 `;
 
 const Content = styled.div`
- max-width: 1128px;
- margin-left: auto;
- margin-right: auto;
+  max-width: 1128px;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const Section = styled.section`
- height: 50px;
- padding: 16px 0;
- box-sizing: content-box;
- text-align: center;
- text-decoration: underline;
- display: flex;
- justify-content: center;
+  min-height: 10px;
+  padding: 16px 0;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+  p {
+    font-size: 14px;
+    color: #434649;
+    font-weight: 600;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
 
- h5{
-      color: #0a66c2;
-      font-size: 14px;
-      a{
-           font-weight: 700;
-      }
- }
-
- p{
-      font-size: 14px;
-      color: #434649;    
-      font-weight: 600;
- }
-
- @media (max-width: 768px){
-     flex-direction: column;
-     padding: 0 5px;
- }
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr), minmax(0, 12fr), minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  grid-template-rows: auto;
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 5px;
+  }
 `;
 
 export default Home;
